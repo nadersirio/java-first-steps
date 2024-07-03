d289d83d8d26e78815e93a9e08f2507b92c10cd8
Náder W. S. Sírio <nadersirio@gmail.com>
2024-07-03 17:17:46 -0300

    att: packages

--- src/nader/primeirasemana/MyClass.java
+++ src/nader/primeirasemana/MyClass.java
@@ -1,10 +1,11 @@
+package nader.primeirasemana;
 public class MyClass {
   public static void main (String [] args) {
-    System.out.println("Me chamo Náder");
-    System.out.println("Tenho 21 anos");
+    System.out.println("My name is Náder");
+    System.out.println("I'm 21 years old");
 
-    final String BR = "Brasil";
-    System.out.println("E eu moro no " + BR);
+    final String BR = "Brazil";
+    System.out.println("I'm Living on " + BR);
 
     String firstName = "Náder";
     String secondName = "Wilk Silva Sírio";
