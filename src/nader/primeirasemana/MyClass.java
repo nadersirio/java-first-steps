583ce8724fcccdd9f63e8afb9c7ea555782cf9c4
Náder W. S. Sírio <nadersirio@gmail.com>
2024-07-08 10:53:33 -0300

    att: data types

--- src/nader/primeirasemana/MyClass.java
+++ src/nader/primeirasemana/MyClass.java
@@ -4,7 +4,7 @@ public class MyClass {
     System.out.println("My name is Náder");
     System.out.println("I'm 21 years old");
 
-    final String BR = "Brazil";
+    final String BR = "Brazil"; // "Final" é a forma de criar uma variável const.
     System.out.println("I'm Living on " + BR);
 
     String firstName = "Náder";
@@ -16,5 +16,16 @@ public class MyClass {
 
   public static String name (String firstName, String secondName) {
     return firstName.concat(" ").concat(secondName);
+  }
+
+  public class DataTypes {
+    public static void main(String[] args) {
+      byte age = 123;
+      short year = 2024;
+      int cep = 21000000; // Se o dado começar com o n° 0, deve-se utilizar outro type.
+      long cpf = 00000000000L; // Deve terminar com L. Se o dado começar com o n° 0, deve-se utilizar outro type.
+      float pi = 3.14F; // Type Float deve-se terminar em F.
+      double cash = 1275.33;
+    }
   }
 }
